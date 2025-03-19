e2dd67645250ad4873589546da1a700492f21f08
Hina Matsubara <hmatsuba@c4r3s5.42tokyo.jp>
2025-03-19 19:15:08 +0900

    norm-ok

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: YourName <your.email@example.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 15:49:29 by YourName          #+#    #+#             */
-/*   Updated: 2025/03/06 13:46:24 by YourName         ###   ########.fr       */
+/*   Updated: 2025/03/19 19:07:03 by YourName         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,226 +15,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <unistd.h>
 
-void	send_char(pid_t server_pid, char c)
+void	send_terminator(int pid)
 {
-	for (int i = 7; i >= 0; i--)
+	static int	i = 0;
+
+	if (i <= 8)
 	{
-		if ((c >> i) & 1)
-			kill(server_pid, SIGUSR2); // 1 を送信
-		else
-			kill(server_pid, SIGUSR1); // 0 を送信
-		usleep(500);                   // 送信間隔を確保
+		if (kill(pid, SIGUSR1) == -1)
+			exit(1);
 	}
+	i++;
 }
 
-int	main(int argc, char *argv[])
+void	send_signal(int pid, char *str)
 {
-	pid_t	server_pid;
-	char	*message;
+	static int	bit = 0;
+	static char	*c = 0;
 
+	if (str)
+		c = str;
+	if (*c)
+	{
+		if ((((unsigned char)*c >> bit) % 2) == 0)
+			if (kill(pid, SIGUSR1) == -1)
+				exit(1);
+		if ((((unsigned char)*c >> bit) % 2) == 1)
+			if (kill(pid, SIGUSR2) == -1)
+				exit(1);
+		bit++;
+		if (bit == 8)
+		{
+			c++;
+			bit = 0;
+		}
+	}
+	if (!(*c))
+		send_terminator(pid);
+}
+
+void	ack_receive(int signal, siginfo_t *info, void *context)
+{
+	static int	id;
+
+	if (info->si_pid != 0)
+		id = info->si_pid;
+	(void)context;
+	if (signal == SIGUSR1)
+		send_signal(id, NULL);
+	if (signal == SIGUSR2)
+		exit(0);
+}
+
+int	main(int argc, char **argv)
+{
+	struct sigaction	sa;
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = ack_receive;
 	if (argc != 3)
 	{
-		ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
-		return (1);
+		ft_printf("format: ./client <PID> <String>\n");
+		exit(1);
 	}
-	server_pid = ft_atoi(argv[1]);
-	message = argv[2];
-	for (int i = 0; message[i] != '\0'; i++)
-	{
-		send_char(server_pid, message[i]);
-	}
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		exit(1);
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		exit(1);
+	send_signal(ft_atoi(argv[1]), argv[2]);
+	while (1)
+		pause();
 	return (0);
 }
-
-// #include "libft/includes/libft.h"
-// #include <signal.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-
-// volatile sig_atomic_t	signal_received = 0;
-
-// void	signal_handler(int sig)
-// {
-// 	(void)sig;
-// 	signal_received = 1;
-// }
-
-// void	send_char(pid_t server_pid, char c)
-// {
-// 	for (int i = 7; i >= 0; i--)
-// 	{
-// 		signal_received = 0;
-// 		if ((c >> i) & 1)
-// 			kill(server_pid, SIGUSR2); // 1 を送信
-// 		else
-// 			kill(server_pid, SIGUSR1); // 0 を送信
-// 		while (!signal_received)       // サーバーからの応答を待つ
-// 			pause();
-// 	}
-// }
-
-// int	main(int argc, char *argv[])
-// {
-// 	pid_t	server_pid;
-// 	char	*message;
-
-// 	if (argc != 3)
-// 	{
-// 		ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
-// 		return (1);
-// 	}
-// 	server_pid = (pid_t)ft_atoi(argv[1]);
-// 	message = argv[2];
-// 	// シグナルハンドラの設定
-// 	signal(SIGUSR1, signal_handler);
-// 	// メッセージの送信
-// 	for (int i = 0; message[i] != '\0'; i++)
-// 		send_char(server_pid, message[i]);
-// 	// 終了を示す NULL 文字を送信
-// 	send_char(server_pid, '\0');
-// 	return (0);
-// }
-
-// void	send_signal(const pid_t pid, char character)
-// {
-// 	int	i;
-
-// 	i = 7;
-// 	while (i >= 0)
-// 	{
-// 		if (character & (1 << i))
-// 			kill(pid, SIGUSR1);
-// 		else
-// 			kill(pid, SIGUSR2);
-// 		i--;
-// 		usleep(100);
-// 	}
-// }
-
-// void	send_str(const pid_t pid, char *str)
-// {
-// 	while (*str)
-// 	{
-// 		send_signal(pid, *str);
-// 		str++;
-// 	}
-// }
-
-// int	main(int argc, char **argv)
-// {
-// 	pid_t		server_pid;
-// 	const char	*message;
-// 	int			i;
-
-// 	if (argc != 3)
-// 		return (1);
-// 	server_pid = ft_atoi(argv[1]);
-// 	send_signal(server_pid, argv[2]);
-// 	return (0);
-// }
-
-// #include "libft/includes/libft.h"
-// #define _POSIX_C_SOURCE 200112L
-// #define _XOPEN_SOURCE 500
-// #include <signal.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-
-// #define PID_ERR_NUM -1
-// #define PID_ERR_MSG "Error: invaled PID"
-// #define CHAR_BIT_LEN 8
-
-// #define USAGE_MSG "Usage: client [server PID] [message]"
-// #define ERR_SEND "Error: faild to send signal"
-
-// void	send_message(pid_t pid, const char *message)
-// {
-// 	unsigned int	bit;
-// 	char			c;
-// 	int				res;
-
-// 	while (*message)
-// 	{
-// 		c = *message++;
-// 		bit = CHAR_BIT_LEN;
-// 		while (bit--)
-// 		{
-// 			if (c & (1 << bit))
-// 				res = kill(pid, SIGUSR2);
-// 			else
-// 				res = kill(pid, SIGUSR1);
-// 			if (res == -1)
-// 			{
-// 				ft_putendl_fd(ERR_SEND, STDIN_FILENO);
-// 				exit(EXIT_FAILURE);
-// 			}
-// 			usleep(100);
-// 		}
-// 	}
-// }
-
-// pid_t	parse_pid(const char *pid_str)
-// {
-// 	pid_t	pid;
-
-// 	if (*pid_str == 0 || !ft_strall(pid_str, ft_isdigit))
-// 		return (PID_ERR_NUM);
-// 	pid = ft_atoi(pid_str);
-// 	if (pid <= 0)
-// 		return (PID_ERR_NUM);
-// 	return (pid);
-// }
-
-// int	main(int argc, char *argv[])
-// {
-// 	pid_t	server_pid;
-
-// 	if (argc != 3)
-// 	{
-// 		ft_putendl_fd(USAGE_MSG, STDIN_FILENO);
-// 		return (EXIT_FAILURE);
-// 	}
-// 	server_pid = parse_pid(argv[1]);
-// 	if (server_pid <= PID_ERR_NUM)
-// 	{
-// 		ft_putendl_fd(PID_ERR_MSG, STDERR_FILENO);
-// 		return (EXIT_FAILURE);
-// 	}
-// 	send_message(server_pid, argv[2]);
-// 	return (EXIT_SUCCESS);
-// }
-
-// void	send_char(pid_t server_pid, char c)
-// {
-// 	for (int i = 7; i >= 0; i--)
-// 	{
-// 		if ((c >> i) & 1)
-// 			kill(server_pid, SIGUSR2); // 1 を送信
-// 		else
-// 			kill(server_pid, SIGUSR1); // 0 を送信
-// 		usleep(100);                   // 送信間隔を確保
-// 	}
-// }
-
-// int	main(int argc, char *argv[])
-// {
-// 	pid_t	server_pid;
-// 	char	*message;
-
-// 	if (argc != 3)
-// 	{
-// 		printf("Usage: %s <server_pid> <message>\n", argv[0]);
-// 		return (1);
-// 	}
-// 	server_pid = atoi(argv[1]);
-// 	message = argv[2];
-// 	for (int i = 0; message[i] != '\0'; i++)
-// 	{
-// 		send_char(server_pid, message[i]);
-// 	}
-// 	return (0);
-// }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: YourName <your.email@example.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 15:49:26 by YourName          #+#    #+#             */
-/*   Updated: 2025/03/10 16:47:41 by YourName         ###   ########.fr       */
+/*   Updated: 2025/03/19 19:07:47 by YourName         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,219 +14,52 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
-void	handle_signal(int sig, siginfo_t *info, void *context)
+void	handle_signal(int signal, siginfo_t *info, void *context)
 {
-	static char	c = 0;
-	static int	bit_count = 0;
+	static unsigned char	val = 0;
+	static int				bit = 1;
+	static int				id = 0;
 
-	(void)info;
 	(void)context;
-	c |= (sig == SIGUSR2) << (7 - bit_count); // SIGUSR2 を 1、SIGUSR1 を 0 としてセット
-	bit_count++;
-	if (bit_count == 8)
+	if (info->si_pid != 0)
+		id = info->si_pid;
+	if (signal == SIGUSR1)
+		val += 0;
+	if (signal == SIGUSR2)
+		val += bit;
+	bit <<= 1;
+	if (bit == 256)
 	{
-		write(1, &c, 1); // 1 文字出力
-		c = 0;
-		bit_count = 0;
+		bit = 1;
+		if (val == 0)
+			if (kill(id, SIGUSR2) == -1)
+				exit(1);
+		if (val != 0)
+			ft_printf("%s", &val);
+		val = 0;
 	}
+	if (kill(id, SIGUSR1) == -1)
+		exit(1);
 }
 
 int	main(void)
 {
+	pid_t				pid;
 	struct sigaction	sa;
 
+	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = handle_signal;
-	sa.sa_flags = SA_SIGINFO; // `siginfo_t` を取得するために設定
-	sigemptyset(&sa.sa_mask);
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-	ft_printf("Server PID: %d\n", getpid());
+	pid = getpid();
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		exit(1);
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		exit(1);
+	ft_printf("PID: ");
+	ft_putnbr(pid);
+	ft_printf("\n");
 	while (1)
-	{
-		pause(); // シグナルを待機
-	}
+		pause();
 	return (0);
 }
-
-// #include "libft/includes/libft.h"
-// #include <signal.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-
-// void	handle_signal(int sig, siginfo_t *info, void *context)
-// {
-// 	static char		c = 0;
-// 	static int		bit_count = 0;
-// 	static pid_t	client_pid = 0;
-
-// 	(void)context;
-// 	if (!client_pid)
-// 		client_pid = info->si_pid; // 最初の送信者を記録
-// 	c |= (sig == SIGUSR2) << (7 - bit_count);
-// 	bit_count++;
-// 	if (bit_count == 8)
-// 	{
-// 		write(1, &c, 1); // 1 文字出力
-// 		c = 0;
-// 		bit_count = 0;
-// 	}
-// 	// クライアントにシグナルを送って次のビット送信を促す
-// 	kill(client_pid, SIGUSR1);
-// }
-
-// int	main(void)
-// {
-// 	struct sigaction	sa;
-
-// 	ft_printf("Server PID: %d\n", getpid()); // 自作 `ft_printf` で PID を表示
-// 	sa.sa_sigaction = handle_signal;
-// 	sa.sa_flags = SA_SIGINFO;
-// 	sigemptyset(&sa.sa_mask);
-// 	sigaction(SIGUSR1, &sa, NULL);
-// 	sigaction(SIGUSR2, &sa, NULL);
-// 	while (1)
-// 	{
-// 		pause(); // シグナルを待機
-// 	}
-// 	return (0);
-// }
-
-// #include "libft/includes/libft.h"
-// #include <stddef.h>
-// // #define _POSIX_C_SOURCE 200809L./client 64871
-// #include <signal.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <unistd.h>
-
-// void	handle_signal(int sig, siginfo_t *info, void *dummy)
-// {
-// 	static int	bit;
-// 	char		c;
-// 	static char	g_char;
-
-// 	(void)info;
-// 	(void)dummy;
-// 	g_char = g_char << 1;
-// 	if (sig == SIGUSR1)
-// 		g_char |= 1;
-// 	else if (sig == SIGUSR2)
-// 		;
-// 	bit++;
-// 	c = 0xff & g_char;
-// 	if (bit == 8)
-// 	{
-// 		write(STDERR_FILENO, &c, 1);
-// 		bit = 0;
-// 	}
-// }
-
-// int	main(void)
-// {
-// 	struct sigaction	act1;
-// 	struct sigaction	act2;
-
-// 	ft_printf("%d\n", getpid());
-// 	ft_memset(&act1, 0, sizeof(sigaction));
-// 	ft_memset(&act2, 0, sizeof(sigaction));
-// 	act1.sa_sigaction = handle_signal;
-// 	act2.sa_sigaction = handle_signal;
-// 	act1.sa_flags = SA_SIGINFO;
-// 	act2.sa_flags = SA_SIGINFO;
-// 	sigemptyset(&act1.sa_mask);
-// 	sigemptyset(&act2.sa_mask);
-// 	sigaddset(&act1.sa_mask, SIGUSR2);
-// 	sigaddset(&act2.sa_mask, SIGUSR1);
-// 	sigaction(SIGUSR1, &act1, NULL);
-// 	sigaction(SIGUSR2, &act2, NULL);
-// 	while (1)
-// 		pause();
-// }
-
-// #include <stddef.h>
-
-// void	handle_signal(int sig, siginfo_t *info, void *dummy)
-// {
-// 	static int	bit;
-// 	char		c;
-
-// 	g_char = g_char << 1;
-// 	if (sig == SIGUSR1)
-// 		g_char |= 1;
-// 	else if (sig == SIGUSR2)
-// 		;
-// 	bit++;
-// 	c = 0xff & g_char;
-// 	if (bit == 8)
-// 	{
-// 		wrte(STDERR_FILENO, &c, 1);
-// 		bit = 0;
-// 	}
-// }
-
-// int	main(void)
-// {
-// 	struct sigaction	act1;
-// 	struct sigaction	act2;
-
-// 	ft_printf("%d\n", getpid());
-// 	ft_memset(&act1, 0, sizeof(sigaction));
-// 	ft_memset(&act2, 0, sizeof(sigaction));
-// 	act1.sa_sigaction = handle_signal;
-// 	act2.sa_sigaction = handle_signal;
-// 	act1.sa_flags = SA_SIGINFO;
-// 	act2.sa_flags = SA_SIGINFO;
-// 	sigaddset(&act1.sa_mask, SIGUSR2);
-// 	sigaddset(&act2.sa_mask, SIGUSR1);
-// 	sigaction(SIGUSR1, &act1, NULL);
-// 	sigaction(SIGUSR2, &act2, NULL);
-// 	while (1)
-// 		pause();
-// }
-
-// #include "libft/includes/libft.h"
-// #define _POSIX_C_SOURCE 200809L
-// #include <signal.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <unistd.h>
-
-// void	handle_signal(int sig, siginfo_t *info, void *context)
-// {
-// 	static char	c = 0;
-// 	static int	bit_count = 0;
-
-// 	(void)info;
-// 	(void)context;
-// 	c |= (sig == SIGUSR2) << (7 - bit_count); // SIGUSR2 を 1、SIGUSR1 を 0 としてセット
-// 	bit_count++;
-// 	if (bit_count == 8)
-// 	{
-// 		write(1, &c, 1); // 1 文字出力
-// 		c = 0;
-// 		bit_count = 0;
-// 	}
-// }
-
-// int	main(void)
-// {
-// 	struct sigaction	sa;
-
-// 	memset(&sa, 0, sizeof(sa));
-// 	sa.sa_sigaction = handle_signal;
-// 	sa.sa_flags = SA_SIGINFO; // `siginfo_t` を取得するために設定
-// 	sigemptyset(&sa.sa_mask);
-// 	sigaction(SIGUSR1, &sa, NULL);
-// 	sigaction(SIGUSR2, &sa, NULL);
-// 	printf("Server PID: %d\n", getpid());
-// 	while (1)
-// 	{
-// 		pause(); // シグナルを待機
-// 	}
-// 	return (0);
-// }
